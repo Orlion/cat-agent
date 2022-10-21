e0b54201f75bb30f59d3fc1cc82bc9a235e93883
wangdongdong <wangdongdong@smzdm.com>
2022-10-21 13:35:13 +0800

    init

--- server/config.go
+++ server/config.go
@@ -12,10 +12,10 @@ func withDefaultConf(config *Config) {
 	}
 
 	if config.ReadTimeoutMillis < 1 {
-		config.ReadTimeoutMillis = 100
+		config.ReadTimeoutMillis = 5000
 	}
 
 	if config.WriteTimeoutMillis < 1 {
-		config.WriteTimeoutMillis = 100
+		config.WriteTimeoutMillis = 5000
 	}
 }
